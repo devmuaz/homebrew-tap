4efc7b8b6cc4e85c03333181c2ec90876f4ed09c
devmuaz <abdulmuaz.571@gmail.com>
2026-09-17 14:08:07 +0300

    Update CoreDeck Cask to version 0.9.1 and improve SHA256 validation in GitHub Actions workflow

--- Casks/coredeck.rb
+++ Casks/coredeck.rb
@@ -1,6 +1,6 @@
 cask "coredeck" do
-    version "0.9.0"
-    sha256 "0eb7dcdc8ca4096cf67e653c44c581000b1ca7f77fbaed217368f0da1e50b0d1"
+    version "0.9.1"
+    sha256 "68cec1fb81e4734106ccc012e32e29c1c683a683922bdbf5b1bc75548d5a5623"
   
     url "https://github.com/devmuaz/CoreDeck/releases/download/v#{version}/coredeck-darwin-arm64.dmg"
     name "CoreDeck"
